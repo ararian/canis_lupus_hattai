ab57a922a8c9aaa84ab10779a82dec3b385fba8e
ararian <example@example.com>
2024-02-13 01:12:24 +0900

    Update:不要な記述を削除し、回路図を更新

--- src/processor/cpu.sv
+++ src/processor/cpu.sv
@@ -5,6 +5,21 @@ module rv_cpu(
     input logic RST
     );
 
+    //インターフェースのインスタンス化
+    hazard hazard(.CLK(CLK), .RST(RST));
+    fetchToDecode fetchToDecode(.CLK(CLK), .RST(RST), .hazard(hazard.fToD));
+    decodeToExecOrDmem decodeToExecOrDmem(.CLK(CLK), .RST(RST), .hazard(hazard.dToED));
+    execToWriteback execToWriteback(.CLK(CLK), .RST(RST));
+    dmemToWriteback dmemToWriteback(.CLK(CLK), .RST(RST));
+    writebackToForward writebackToForward(.CLK(CLK), .RST(RST), .hazard(hazard.wToF));
+
+    //モジュールのインスタンス化
+    fetcher fetcher(.writebackToForward(writebackToForward.fetch), .fetchToDecode(fetchToDecode.fetch));
+    decoder decoder(.fetchToDecode(fetchToDecode.decode), .decodeToExecOrDmem(decodeToExecOrDmem.decode));
+    exec exec(.writebackToForward(writebackToForward.exec), .decodeToExecOrDmem(decodeToExecOrDmem.execOrDmem), .execToWriteback(execToWriteback.exec), .hazard(hazard.exec));
+    memaccess memaccess(.writebackToForward(writebackToForward.dmem), .decodeToExecOrDmem(decodeToExecOrDmem.execOrDmem), .dmemToWriteback(dmemToWriteback.dmem));
+    writeback writeback(.execToWriteback(execToWriteback.writeback), .dmemToWriteback(dmemToWriteback.writeback), .writebackToForward(writebackToForward.writeback));
+    
     //デコード命令
     logic[6:0] opcode;
     logic[4:0] rd;
@@ -14,29 +29,6 @@ module rv_cpu(
     logic[7:0] funct7;
     logic[BIN_DIG-1:0] imm;
 
-    // //制御ハザード用フラグ
-    // logic branch, jamp;
-    // logic ctrl_hazard;
-
-    // assign ctrl_hazard = branch | jamp;
-
-    //インターフェースのインスタンス化
-    hazard hazard(.CLK(CLK), .RST(RST));
-    fetchToDecode fetchToDecode(.CLK(CLK), .RST(RST), .hazard(hazard.fToD));
-    decodeToExecOrDmem decodeToExecOrDmem(.CLK(CLK), .RST(RST), .hazard(hazard.dToED));
-    execToWriteback execToWriteback(.CLK(CLK), .RST(RST));
-    topToExecOrDmem topToExecOrDmem(.CLK(CLK), .RST(RST));
-    dmemToWriteback dmemToWriteback(.CLK(CLK), .RST(RST));
-    writebackToForward writebackToForward(.CLK(CLK), .RST(RST), .hazard(hazard.wToF));
-
-
-    //モジュールのインスタンス化
-    fetcher fetcher(.writebackToForward(writebackToForward.fetch), .fetchToDecode(fetchToDecode.fetch));
-    decoder decoder(.fetchToDecode(fetchToDecode.decode), .decodeToExecOrDmem(decodeToExecOrDmem.decode));
-    exec exec(.writebackToForward(writebackToForward.exec), .decodeToExecOrDmem(decodeToExecOrDmem.execOrDmem), .execToWriteback(execToWriteback.exec), .hazard(hazard.exec));
-    memaccess memaccess(.writebackToForward(writebackToForward.dmem), .decodeToExecOrDmem(decodeToExecOrDmem.execOrDmem), .dmemToWriteback(dmemToWriteback.dmem));
-    writeback writeback(.execToWriteback(execToWriteback.writeback), .dmemToWriteback(dmemToWriteback.writeback), .writebackToForward(writebackToForward.writeback));
-    
     //実行ログ確認用の配線
     assign opcode = decodeToExecOrDmem.next_opcode;
     assign rd = decodeToExecOrDmem.next_rd;
